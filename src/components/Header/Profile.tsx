b252fbffb586b713668273d0d9cce6abfd207976
Marc <marcelopgm65@gmail.com>
2021-04-03 16:39:18 -0300

    :iphone: refact: Header responsive design

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Marcelo Silva</Text>
-        <Text color="gray.300" fontSize="small">
-          marc@email.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Marcelo Silva</Text>
+          <Text color="gray.300" fontSize="small">
+            marc@email.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Marcelo Silva" src="https://github.com/GH-Marc.png" />
     </Flex>
