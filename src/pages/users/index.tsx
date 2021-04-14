7a60d65ea6bbb630a0710add13a1f7b291765261
Marc <marcelopgm65@gmail.com>
2021-04-14 16:17:22 -0300

    :wrench: chore: React-query configuration

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,17 +1,42 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+  useBreakpointValue,
+  Spinner,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import Head from "next/head";
+
+import { useQuery } from "react-query";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import Link from 'next/link';
-import Head from 'next/head';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
-  })
+  });
 
   return (
     <>
@@ -27,10 +52,12 @@ export default function UserList() {
 
           <Box flex="1" borderRadius={8} bg="gray.800" p="8">
             <Flex mb="8" justify="space-between" align="center">
-              <Heading size="lg" fontWeight="normal">Usuários</Heading>
+              <Heading size="lg" fontWeight="normal">
+                Usuários
+              </Heading>
 
               <Link href="/users/create" passHref>
-                <Button 
+                <Button
                   as="a"
                   size="sm"
                   fontSize="sm"
@@ -42,50 +69,65 @@ export default function UserList() {
               </Link>
             </Flex>
 
-            <Table colorScheme="whiteAlpha">
-              <Thead>
-                <Tr>
-                  <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                    <Checkbox colorScheme="pink" />
-                  </Th>
-                  <Th>Usuário</Th>
-                  { isWideVersion && <Th>Data de cadastro</Th> }
-                  <Th width="8"></Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td px={["4", "4", "6"]}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Marcelo Silva</Text>
-                      <Text fontSize="sm" color="gray.300">marc@email.com</Text>
-                    </Box>
-                  </Td>
-                  { isWideVersion && <Td>02 de Abril, 2021</Td> }
+            {isLoading ? (
+              <Flex justify="center">
+                <Spinner />
+              </Flex>
+            ) : error ? (
+              <Flex justify="center">
+                <Text>Falha ao obter dados dos usuários</Text>
+              </Flex>
+            ) : (
+              <>
+                <Table colorScheme="whiteAlpha">
+                  <Thead>
+                    <Tr>
+                      <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                        <Checkbox colorScheme="pink" />
+                      </Th>
+                      <Th>Usuário</Th>
+                      {isWideVersion && <Th>Data de cadastro</Th>}
+                      <Th width="8"></Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">Marcelo Silva</Text>
+                          <Text fontSize="sm" color="gray.300">
+                            marc@email.com
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>02 de Abril, 2021</Td>}
 
-                  { isWideVersion ? 
-                  <Td>
-                    <Button 
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  </Td> : ''}
-                </Tr>
-              </Tbody>
-            </Table>
+                      {isWideVersion ? (
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="pink"
+                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                          >
+                            Editar
+                          </Button>
+                        </Td>
+                      ) : null}
+                    </Tr>
+                  </Tbody>
+                </Table>
 
-            <Pagination />
+                <Pagination />
+              </>
+            )}
           </Box>
         </Flex>
       </Box>
     </>
-  )
+  );
 }
